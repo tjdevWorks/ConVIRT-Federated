4f672d9d213f7a280311e5feb70af91c208a4fca
Tejas Mahajan <tejas.mahajan121@gmail.com>
2022-12-18 01:49:51 -0500

    flwr script initial commit

---
+++ src/flwr_client.py
@@ -0,0 +1,187 @@
+from typing import Dict, List, Callable, Optional, Tuple
+from collections import OrderedDict
+import os
+from path import Path
+
+import hydra
+from hydra.core.hydra_config import HydraConfig
+from hydra import compose, initialize
+from omegaconf import OmegaConf
+import flwr as fl
+from flwr.common.typing import Scalar
+import ray
+import torch
+import numpy as np
+from omegaconf import DictConfig
+import pytorch_lightning as pl
+from pytorch_lightning import Callback, Trainer
+from pytorch_lightning.loggers import LightningLoggerBase
+
+from src import utils
+
+# Flower client, adapted from Pytorch quickstart example
+class FlowerClient(fl.client.NumPyClient):
+    def __init__(self, cid: str, config_filename: str, indices: List, hydra_runtime_log_dir):
+        ## TODO: Think how can you have the clean up function setup like in utils.task_wrapper
+        self.cid = cid
+
+        ## Load the hydra config
+        cfg = self.load_client_config(config_filename)
+
+        ## Reset certain cfg values
+        cfg = self.reset_specific_cfg_values(cfg, hydra_runtime_log_dir)
+
+        ## set seed for random number generators in pytorch, numpy and python.random
+        if cfg.get("seed"):
+            pl.seed_everything(cfg.seed, workers=True)
+
+        ## Create client directory if it doesn't exist
+        if not os.path.exists(f"{hydra_runtime_log_dir}/{cid}"):
+            os.mkdir(f"{hydra_runtime_log_dir}/{cid}")
+
+        ## Instantiate model
+        self.model = hydra.utils.instantiate(cfg.model)
+
+        ## Create Data Module and call setup
+        self.datamodule = hydra.utils.instantiate(cfg.datamodule)
+        self.datamodule.setup("fit", indices=indices, debug_mode=cfg.datamodule_debug_mode)
+
+        #print(f"Created all objects for : {self.cid}")
+        
+        self.cfg = cfg
+
+        # Determine device
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    
+    def load_client_config(self, config_fname):
+        #print(f"Client: {self.cid}")
+        hydra.core.global_hydra.GlobalHydra.instance().clear()
+        initialize(version_base="1.2", config_path="../configs/")
+        cfg = compose(config_name=config_fname, return_hydra_config=True)
+        HydraConfig().cfg = cfg
+        OmegaConf.resolve(cfg)
+        return cfg
+
+    def reset_specific_cfg_values(self, cfg, runtime_dir):
+        ## Paths output_dir
+        cfg['paths']['output_dir'] = str(Path(runtime_dir) / f'{self.cid}')
+        ## logger csv save dir
+        cfg['logger']['csv']['save_dir'] = str(Path(runtime_dir) / f'{self.cid}')
+        ## callbacks model checkpoint
+        cfg['callbacks']['model_checkpoint']['dirpath'] = str(Path(runtime_dir) / f'{self.cid}')
+        ## Trainer default_root_dir
+        cfg['trainer']['default_root_dir'] = str(Path(runtime_dir) / f'{self.cid}')
+        return cfg
+    
+    def set_server_round_dir(self, server_round):
+        ## logger csv save dir
+        self.cfg['logger']['csv']['save_dir'] = str(Path(self.cfg['logger']['csv']['save_dir']) / f"{server_round}")
+        ## callbacks model checkpoint
+        self.cfg['callbacks']['model_checkpoint']['dirpath'] = str(Path(self.cfg['callbacks']['model_checkpoint']['dirpath']) / f"{server_round}/checkpoints/")
+        
+
+    def get_parameters(self, config):
+        return get_params(self.model)
+
+    def fit(self, parameters, config):
+        ## Set the directories
+        self.set_server_round_dir(config["server_round"])
+
+        self.callbacks: List[Callback] = utils.instantiate_callbacks(self.cfg.callbacks)
+
+        self.logger: List[LightningLoggerBase] = utils.instantiate_loggers(self.cfg.get("logger"))
+
+        #print(f"Creating trainer object, {self.cfg.trainer.default_root_dir, type(self.cfg.trainer.default_root_dir)}")
+        
+        ## Create Trainer Object
+        self.trainer: Trainer = hydra.utils.instantiate(self.cfg.trainer, callbacks=self.callbacks, logger=self.logger)
+
+        ## For model checkpoint change output directory
+        if type(self.callbacks[0])==pl.callbacks.ModelCheckpoint:
+            os.makedirs(self.callbacks[0].dirpath, exist_ok=True)
+
+        ## For csv logging change save directory
+        if type(self.logger[0])==pl.loggers.csv_logs.CSVLogger:
+            os.makedirs(self.logger[0].save_dir, exist_ok=True)
+
+        set_params(self.model, parameters)
+
+        # Load data for this client and get trainloader
+        num_workers = int(ray.get_runtime_context().get_assigned_resources()["CPU"])
+        
+        ## Create data loaders
+        train_dataloader = self.datamodule.train_dataloader(num_workers=num_workers)
+        val_dataloader = self.datamodule.val_dataloader(num_workers=num_workers)
+        
+        self.trainer.fit(model=self.model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
+
+        # Return local model and statistics
+        return get_params(self.model), len(train_dataloader), self.trainer.callback_metrics
+
+    def evaluate(self, parameters, config):
+        ## Set the directories
+        #print(f"Config: {config}\t{self.cfg['callbacks']['model_checkpoint']['dirpath']}")
+        #self.set_server_round_dir(config["server_round"])
+
+        self.callbacks: List[Callback] = utils.instantiate_callbacks(self.cfg.callbacks)
+
+        self.logger: List[LightningLoggerBase] = utils.instantiate_loggers(self.cfg.get("logger"))
+
+        ## Create Trainer Object
+        self.trainer: Trainer = hydra.utils.instantiate(self.cfg.trainer, callbacks=self.callbacks, logger=self.logger)
+
+        set_params(self.model, parameters)
+
+        # Load data for this client and get trainloader
+        num_workers = int(ray.get_runtime_context().get_assigned_resources()["CPU"])
+        
+        val_dataloader = self.datamodule.val_dataloader(num_workers=num_workers)
+        
+        _ = self.trainer.validate(model=self.model, dataloaders=val_dataloader)
+
+        return self.trainer.callback_metrics["val/loss"].item(), len(val_dataloader), self.trainer.callback_metrics
+    
+
+def get_params(model):
+    return [val.cpu().numpy() for _, val in model.state_dict().items()]
+
+
+def set_params(model, parameters):
+    params_dict = zip(model.state_dict().keys(), parameters)
+    state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+    model.load_state_dict(state_dict, strict=True)
+
+
+def get_evaluate_fn(
+    cfg: DictConfig,
+) -> Callable[[fl.common.NDArrays], Optional[Tuple[float, float]]]:
+    """Return an evaluation function for centralized evaluation."""
+
+    def evaluate(
+        server_round: int, parameters: fl.common.NDArrays, config: Dict[str, Scalar]
+    ) -> Optional[Tuple[float, float]]:
+        #print(f"Server Round: {server_round}")
+        ## Instantiate model
+        model = hydra.utils.instantiate(cfg.model)
+        
+        set_params(model, parameters)
+
+        ## Create Data Module and call setup
+        datamodule = hydra.utils.instantiate(cfg.datamodule)
+        datamodule.setup("test")
+
+        #cfg['callbacks']['model_checkpoint']['dirpath'] += '/checkpoints/' 
+        #callbacks: List[Callback] = utils.instantiate_callbacks(cfg.callbacks)
+        #os.makedirs(callbacks[0].dirpath, exist_ok=True)
+
+        logger: List[LightningLoggerBase] = utils.instantiate_loggers(cfg.get("logger"))
+
+        trainer: Trainer = hydra.utils.instantiate(cfg.trainer, logger=logger)#, callbacks=callbacks)
+
+        ## Trainer Test Function
+        trainer.test(model=model, datamodule=datamodule)
+        
+        # return statistics
+        return None, trainer.callback_metrics
+
+    return evaluate
---
+++ src/run_simulation.py
@@ -0,0 +1,84 @@
+import pyrootutils
+
+root = pyrootutils.setup_root(
+    search_from=__file__,
+    indicator=[".git", "pyproject.toml"],
+    pythonpath=True,
+    dotenv=True,
+)
+
+from typing import Dict, Optional, Callable
+
+import hydra
+from omegaconf import DictConfig
+import flwr as fl
+from flwr.common.typing import Scalar
+import numpy as np
+from utils.data_split import process_traindata, partition_class, partition_volume
+from pytorch_lightning import Callback, Trainer
+import pytorch_lightning as pl
+
+from src import utils
+from src.flwr_client import FlowerClient, get_evaluate_fn
+
+def fit_config(server_round: int) -> Dict[str, Scalar]:
+    """Return a configuration with static batch size and (local) epochs."""
+    config = {
+        # "epochs": 1,  # number of local epochs
+        # "batch_size": 32,
+        "server_round": server_round
+    }
+    return config
+
+@utils.task_wrapper
+def sim_run(cfg: DictConfig):
+    # set seed for random number generators in pytorch, numpy and python.random
+    if cfg.get("seed"):
+        pl.seed_everything(cfg.seed, workers=True)
+
+    ## Number of dataset partions (= number of total clients)
+    pool_size = cfg.pool_size 
+    
+    ## Data partitioning
+    dataframe = process_traindata(cfg.partitions.train_path, cfg.partitions.policy)
+
+    if cfg.partitions.key == 'volume':
+        partitions = partition_volume(dataframe, cfg.partitions.mode, cfg.partitions.num_clients, cfg.partitions.scale)
+    elif cfg.partitions.key == 'class':
+        partitions = partition_class(dataframe, cfg.partitions.modeparams, cfg.partitions.mode, cfg.partitions.num_clients, cfg.partitions.exclusive, cfg.partitions.equal_num_samples, cfg.partitions.min_client_samples)     
+    
+    ## Configure the strategy
+    strategy = hydra.utils.instantiate(cfg.strategy)
+    strategy.evaluate_fn = get_evaluate_fn(cfg)
+    strategy.on_fit_config_fn = fit_config
+
+    hydra_runtime_log_dir = hydra.core.hydra_config.HydraConfig.get()['runtime']['output_dir']
+
+    def client_fn(cid: str):
+        # create a single client instance
+        return FlowerClient(cid, 'client/fed_client', partitions[int(cid)], hydra_runtime_log_dir)
+    
+    server_config = hydra.utils.instantiate(cfg.server_config)
+
+    ## Start Simulation
+    fl.simulation.start_simulation(
+        client_fn=client_fn,
+        num_clients=pool_size,
+        client_resources=cfg.client_resources,
+        config=server_config,
+        strategy=strategy,
+        ray_init_args=cfg.ray_init_args,
+    )
+
+    return None, None
+
+@hydra.main(version_base="1.2", config_path=root / "configs", config_name="simulation")
+def main(cfg: DictConfig) -> Optional[float]:
+    
+    _ = sim_run(cfg)
+
+    return "YOLO"
+    
+if __name__ == "__main__":
+    main()
+    
